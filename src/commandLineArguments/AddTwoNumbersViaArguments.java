a5cee71aad94f1b30499433d194c5a8c9e74da22
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2026-02-17 11:53:41 +0530

    command line arguments assignment done

---
+++ src/commandLineArguments/AddTwoNumbersViaArguments.java
@@ -0,0 +1,17 @@
+package commandLineArguments;
+
+public class AddTwoNumbersViaArguments {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Please provide two numbers.");
+            return;
+        }
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum = " + sum);
+    }
+}
